4faf000d7d56d1560678c3395a1a3346a6ba252f
Thomas Wagner <thomaswagner@fh-muenster.de>
2023-08-28 12:32:49 +0200

    Integrated new Images

--- SchachGUI.java
+++ SchachGUI.java
@@ -8,17 +8,9 @@ import java.io.IOException;
 public class SchachGUI extends JFrame {
     private final int SIZE = 8;
     private final int CELL_SIZE = 80;
-    private final String IMAGE_PATH = "C:\\Java-Projekte\\SchachFrontend\\imgs";
+    private final String IMAGE_PATH = "./assets/";
 
-    private final String[] WEISS_FIGUREN_BILDER = {
-            "turm_w.png", "sprienger_w.png", "laeufer_w.png", "koenig_w.png",
-            "dame_w.png", "bauer_w.png"
-    };
-
-    private final String[] SCHWARZ_FIGUREN_BILDER = {
-            "turm_s.png", "sprienger_s.png", "laeufer_s.png", "koenig_s.png",
-            "dame_s.png", "bauer_s.png"
-    };
+    private final String[] FIGUREN_BILDER = {"rook", "knight", "bishop", "king", "queen", "pawn"};
 
     private final JPanel schachbrettPanel;
     private final JPanel infoPanel;
@@ -46,6 +38,10 @@ public class SchachGUI extends JFrame {
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(SchachGUI::new);
     }
 
     private void initializeSchachbrett() {
@@ -76,30 +72,9 @@ public class SchachGUI extends JFrame {
         };
         for (int y = 0; y < board.length; y++) {
             for (int x = 0; x < board[y].length; x++) {
-                String bildDatei = "";
-                switch (board[y][x].piecetype) {
-                    case ROOK:
-                        bildDatei = board[y][x].color == ChessColor.WHITE ? WEISS_FIGUREN_BILDER[0] : SCHWARZ_FIGUREN_BILDER[0];
-                        break;
-                    case KNIGHT:
-                        bildDatei = board[y][x].color == ChessColor.WHITE ? WEISS_FIGUREN_BILDER[1] : SCHWARZ_FIGUREN_BILDER[1];
-                        break;
-                    case BISHOP:
-                        bildDatei = board[y][x].color == ChessColor.WHITE ? WEISS_FIGUREN_BILDER[2] : SCHWARZ_FIGUREN_BILDER[2];
-                        break;
-                    case KING:
-                        bildDatei = board[y][x].color == ChessColor.WHITE ? WEISS_FIGUREN_BILDER[3] : SCHWARZ_FIGUREN_BILDER[3];
-                        break;
-                    case QUEEN:
-                        bildDatei = board[y][x].color == ChessColor.WHITE ? WEISS_FIGUREN_BILDER[4] : SCHWARZ_FIGUREN_BILDER[4];
-                        break;
-                    case PAWN:
-                        bildDatei = board[y][x].color == ChessColor.WHITE ? WEISS_FIGUREN_BILDER[5] : SCHWARZ_FIGUREN_BILDER[5];
-                        break;
-                }
+                String bildDatei = getBildDatei(board, y, x);
                 schachbrett[y][x] = bildDatei;
             }
-            System.out.println();
         }
 
         for (int row = 0; row < SIZE; row++) {
@@ -130,6 +105,37 @@ public class SchachGUI extends JFrame {
         }
     }
 
+    private String getBildDatei(Piece[][] board, int y, int x) {
+        String bildDatei = "";
+        if (board[y][x] != null) {
+            switch (board[y][x].piecetype) {
+                case ROOK:
+                    bildDatei = FIGUREN_BILDER[0];
+                    break;
+                case KNIGHT:
+                    bildDatei = FIGUREN_BILDER[1];
+                    break;
+                case BISHOP:
+                    bildDatei = FIGUREN_BILDER[2];
+                    break;
+                case KING:
+                    bildDatei = FIGUREN_BILDER[3];
+                    break;
+                case QUEEN:
+                    bildDatei = FIGUREN_BILDER[4];
+                    break;
+                case PAWN:
+                    bildDatei = FIGUREN_BILDER[5];
+                    break;
+            }
+            bildDatei += " " + (board[y][x].color == ChessColor.WHITE ? "w" : "b");
+            bildDatei += " " + ((y + x) % 2 == 0 ? "w" : "b");
+        } else {
+            bildDatei = (y + x) % 2 == 0 ? "white" : "black";
+        }
+        return bildDatei;
+    }
+
     private void handleCellClick(JButton button) {
         if (selectedButton == null) {
             selectedButton = button;
@@ -145,9 +151,5 @@ public class SchachGUI extends JFrame {
 
     private Color getCellColor(int row, int col) {
         return (row + col) % 2 == 0 ? new Color(245, 245, 220) : new Color(139, 69, 19);
-    }
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(SchachGUI::new);
     }
 }
